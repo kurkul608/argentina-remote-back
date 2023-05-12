993c16963d26548fadb0718264edc9bf3237738c
Solpio <ssusolpio@gmail.com>
2023-05-12 15:44:34 +0400

    https://argentinadev.atlassian.net/browse/RA-69

--- src/setting/dto/update-settings.dto.ts
+++ src/setting/dto/update-settings.dto.ts
@@ -1,8 +1,12 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsBoolean, IsOptional, ValidateNested } from 'class-validator';
-import { Type } from 'class-transformer';
-import { CleanServiceMessageBodyDto } from 'src/setting/dto/body/clean-service-message-body.dto';
+import { Transform, Type } from 'class-transformer';
+import {
+  CleanServiceMessageBodyDto,
+  CleanServiceMessageDto,
+} from 'src/setting/dto/body/clean-service-message-body.dto';
 import { IsCustomObject } from 'src/commoon/decorators/validate-custom-object.decorator';
+import { MessageType } from 'src/setting/settings.schema';
 
 export class UpdateSettingsDto {
   @ApiProperty({
@@ -16,26 +20,24 @@ export class UpdateSettingsDto {
 
   @ApiProperty({
     example: {
-      new_member: true,
-      video_call_end: false,
-      video_call_start: false,
-      pinned_message: false,
-      auto_delete_timer_changed: false,
-      left_member: false,
+      clear_all: true,
+      message_types: [
+        'new_member',
+        'left_member',
+        'video_call_start',
+        'video_call_end',
+        'auto_delete_timer_changed',
+        'pinned_message',
+      ],
     },
     description: 'Should bot remove system notification',
     required: false,
   })
   @IsOptional()
   @ValidateNested()
-  @Type(() => CleanServiceMessageBodyDto)
-  @IsCustomObject({
-    new_member: true,
-    video_call_end: false,
-    video_call_start: false,
-    pinned_message: false,
-    auto_delete_timer_changed: false,
-    left_member: false,
-  })
-  clear_system_messages?: CleanServiceMessageBodyDto;
+  @Type(() => CleanServiceMessageDto)
+  clear_system_messages?: {
+    clear_all: boolean;
+    message_types: MessageType[];
+  };
 }
